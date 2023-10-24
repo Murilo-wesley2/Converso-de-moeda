b17b01ae784f0ecd8041f37e87cecce2de56f0aa
murilowesle2 <murilowesley0802@gmail.com>
2023-10-23 22:48:22 -0300

    o converte dolar em real

---
+++ converso_de_moeda.py
@@ -0,0 +1,30 @@
+import requests
+import PySimpleGUI as sg
+
+#Layout
+sg.theme('Reddit')
+layout=[
+        [sg.Text('Real'),sg.Input(key='real')],
+        [sg.Button('Converte')],
+        [sg.Output(size=(10,3),key='saida')]
+]
+
+#Janela
+janela=sg.Window('conversor de moeda',layout)
+
+#Ler eventos
+while True:
+    try:
+        requisicao=requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+    
+    except:
+        break
+    
+    else:
+        eventos, valores=janela.read()
+        cotacao=requisicao.json()
+        if eventos==sg.WINDOW_CLOSED:
+            break
+        if eventos=='Converte':
+            valor=float(cotacao['USDBRL']['bid'])*float(valores['real'])
+            print(valor)
