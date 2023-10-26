bc6b7ee1048fbd879b181f815132d36081911d74
* <murilowesley0802@gmail.com>
2023-10-26 15:55:29 -0300

    novas moedas adicionadas e erros corrigidos

--- converso_de_moeda.py
+++ converso_de_moeda.py
@@ -5,7 +5,7 @@ import PySimpleGUI as sg
 sg.theme('Reddit')
 layout=[
         [sg.Text('Real'),sg.Input(key='real')],
-        [sg.Button('Converte')],
+        [sg.Button('USD'), sg.Button('EUR'), sg.Button('BTC')],
         [sg.Text('',key='saida')]
 ]
 
@@ -23,8 +23,22 @@ while True:
     else:
         eventos, valores=janela.read()
         cotacao=requisicao.json()
+        
+        if valores['real']=='':
+            valores['real']='0'
         if eventos==sg.WINDOW_CLOSED:
             break
-        if eventos=='Converte':
-            valor=float(valores['real'])/float(cotacao['USDBRL']['bid'])
-            janela['saida'].update(f'US$ {valor:.2f}')
+        if valores['real'].isnumeric()==True:
+            if eventos=='USD':
+                moeda='USDBRL'
+                simbolo='US$'
+            elif eventos=='EUR':
+                moeda='EURBRL'
+                simbolo='€'
+            elif eventos=='BTC':
+                moeda='BTCBRL'
+                simbolo='₿'
+            valor=float(valores['real'])/float(cotacao[moeda]['bid'])
+            janela['saida'].update(f'{simbolo} {valor:.2f}')
+        else:
+            janela['saida'].update('digite apenas numeros')
